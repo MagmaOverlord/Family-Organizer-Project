c897a41612ae5ccd0a5286c127d121112e025a92
Tomas Brejl <tomas.brejl@outlook.com>
2021-03-09 23:17:28 -0800

    added signup and login

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect, session, render_template
 app = Flask(__name__)
 
+import mysql.connector
+
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
@@ -9,8 +11,8 @@ app.secret_key = "Key Time!"
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="tomas5555",
-    database="big-project"
+    password="greatkid5000",
+    database="sys"
 )
 
 cursor = db.cursor()
@@ -21,7 +23,49 @@ db.commit()
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    if "username" in session:
+        return render_template("welcome.html", username = session["username"], list="")
+    else:
+        return render_template("index.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        sql = "SELECT username FROM users"
+        cursor.execute(sql)
+        usernames = cursor.fetchall()
+        sql = "SELECT password FROM users"
+        cursor.execute(sql)
+        passwords = cursor.fetchall()
+        i = 0
+        for user in usernames:
+            if username == user and password == password[i]:
+                    session["username"] = username
+                    return redirect("/home")
+            i+=1
+            return render_template("index.html", message = "Wrong username or password.")
+    else:
+        return render_template("index.html")
+
+@app.route("/signup", methods = ["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmPassword = request.form.get("confirm-password")
+        if password == confirmPassword:
+            sql = f'INSERT INTO users ("{username}", "{password}")'
+            cursor.execute(sql)
+            db.commit()
+    else:
+        return render_template("signup.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 if __name__ == '__main__':
     import os
